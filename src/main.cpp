133235cf316a419e87efafee84767f1ab0eee858
Oscar Maia <oscarfgmaia@gmail.com>
2024-06-06 11:09:44 -0300

    remove tests

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,6 @@
 #include "Vec2.h"
 
 int main() {
-  Vec2 a = Vec2(100, 200);
-  Vec2 b = Vec2(400, 700);
-  a.normalized(b);
-  std::cout << a.normalized(b).y;
   Game game("./config/config.txt");
   game.run();
 }
